7a2e21855586a8ba0a992f4e56688e0c9745b0ee
WubbaLubbaDubDubDev <kolagula13@gmail.com>
2024-09-28 11:22:31 +0300

    Changed values of several parameters in the configuration file

--- bot/config/config.py
+++ bot/config/config.py
@@ -7,16 +7,18 @@ class Settings(BaseSettings):
     API_ID: int
     API_HASH: str
 
-    SLEEP_TIME: list[int] = [2700, 4200]
-    START_DELAY: list[int] = [5, 100]
+    SLEEP_TIME: list[int] = [360, 3600]
+    START_DELAY: list[int] = [5, 25]
     AUTO_TASK: bool = True
-    TASKS_TO_DO: list[str] = ["paint20pixels", "leagueBonusSilver"]
+    TASK_TO_DO: list[str] = ["x:notcoin", "x:notpixel", "paint20pixels", "leagueBonusSilver", "invite3frens",
+                              "leagueBonusGold"]
     AUTO_DRAW: bool = True
     JOIN_TG_CHANNELS: bool = True
     CLAIM_REWARD: bool = True
     AUTO_UPGRADE: bool = True
-    REF_ID: str = 'f464869246'
-    IGNORED_BOOSTS: list[str] = ['paintReward']
+    REF_ID: str = 'f411905106'
+    IGNORED_BOOSTS: list[str] = ['INVITE_FRIENDS', 'TON_TRANSACTION', 'BOOST_CHANNEL', 'ACTIVITY_CHALLENGE',
+                                 'CONNECT_WALLET']
 
 
 settings = Settings()
