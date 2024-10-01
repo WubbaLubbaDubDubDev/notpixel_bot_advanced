4931e22c5a2ed5a838d95ab026b308f627467664
WubbaLubbaDubDubDev <kolagula13@gmail.com>
2024-10-01 08:48:49 +0300

    Updated headers due to domain change

--- bot/core/headers.py
+++ bot/core/headers.py
@@ -3,19 +3,19 @@ headers = {
     'Accept-Language': 'ru,en;q=0.9,en-GB;q=0.8,en-US;q=0.7',
     'Accept-Encoding': 'gzip, deflate, br, zstd',
     'Content-Type': 'application/json',
-    'Origin': 'https://image.notpx.app',
-    'Referer': 'https://image.notpx.app/',
+    'Origin': 'https://app.notpx.app',
+    'Referer': 'https://app.notpx.app/',
     'Sec-Fetch-Dest': 'empty',
     'Sec-Fetch-Mode': 'cors',
     'Sec-Fetch-Site': 'same-site',
     'Sec-Ch-Ua': '"Chromium";v="128", "Not;A=Brand";v="24", "Microsoft Edge";v="128", "Microsoft Edge WebView2";v="128"',
     'Sec-Ch-Ua-mobile': '?0',
     'Sec-Ch-Ua-platform': '"Android"',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.165 Mobile Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 11; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36'
 }
 
 headers_squads = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 11; Pixel 4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Mobile Safari/537.36",
     "Accept": "*/*",
     "Accept-Language": "en-US,en;q=0.5",
     "Accept-Encoding": "gzip, deflate, br, zstd",
